bf6305429d452bae573bf82eee081ed667c3d5a4
webtech20 <webtech20@5f668ee0-2ad8-11df-b279-ed2be448802f>
2010-10-08 01:50:09 +0000

    fixed problems with the image tag

--- daisyCmsPlugin/grails-app/controllers/com/daisyPlugin/controller/DaisyController.groovy
+++ daisyCmsPlugin/grails-app/controllers/com/daisyPlugin/controller/DaisyController.groovy
@@ -1,26 +1,34 @@
 package com.daisyPlugin.controller
+import com.daisyPlugin.daisyIntegration.service.*;
 class DaisyController {
 def daisyService
     def index = { }
    def contentHandler={
 	    def theId=params.id
-		def documentType=daisyService.getDocumentType(theId)
-		if (documentType=="Image"){
-			daisyService.doImage(theId,response.getOutputStream())
+		if (theId!=null){
+		  DaisyService ds=daisyService
+		  def documentType=ds.getDocumentType(theId)
+		  if (documentType=="Image"){
+			ds.doImage(theId,response.getOutputStream())
 			response.setContentType("image/jpeg")
-		}else
-		if (documentType=="SimpleDocument"){
-			osw.write(daisyService.getHtmlDocumentFromId(theId))
+		  }else
+		  if (documentType=="SimpleDocument"){
+			def osw=new OutputStreamWriter(osb,"UTF-8")
+			def osb=new BufferedOutputStream(response.getOutputStream())
+			osw.write(ds.getHtmlContent(theId))
+		  }
 		}
       }
    def contentList={
 	   def documentName=params.documentName
 	   def documentList=params.fieldType
-	   def lst=daisyService.getFields(documentName,fieldType)
-	   def osb=new BufferedOutputStream(response.getOutputStream())
-       def osw=new OutputStreamWriter(osb,"UTF-8")
-	   lst.each{
+	   if (documentName!=null && documentList!=null){
+	     def lst=daisyService.getFields(documentName,fieldType)
+	     def osb=new BufferedOutputStream(response.getOutputStream())
+         def osw=new OutputStreamWriter(osb,"UTF-8")
+	     lst.each{
 		   osw.write("<option value=\""+it+"\">"+it+"</option>")
+	     }
 	   }
      }
 }
---
+++ daisyCmsPlugin/grails-app/taglib/com/daisyPlugin/daisy/tag/DaisyCmsTagLib.groovy
@@ -0,0 +1,20 @@
+package com.daisyPlugin.daisy.tag
+import com.daisyPlugin.daisyIntegration.service.*;
+public class DaisyCmsTagLib{
+
+		static namespace = 'daisyCms'
+		def daisyService
+		def img={attrs->
+         def id=attrs["id"]
+		 def style=attrs["style"]
+	     def t='<img src="/daisyController/contentHandler?id="'+id+'" style="'+style+'" />' 
+	     out << t
+		}
+	   def simpleText={attrs->
+		DaisyService ds=daisyService
+		def id=attrs["id"]
+		def style=attrs["style"]
+		def t='<div style="'+style+'">'+ds.getHtmlContent(id)+'<div/>' 
+		out << t
+  	}
+}	
