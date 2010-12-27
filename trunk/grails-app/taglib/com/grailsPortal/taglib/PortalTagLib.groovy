4e50205bba02fbb8dae7efed1402622ca56cede6
webtech20 <webtech20@5f668ee0-2ad8-11df-b279-ed2be448802f>
2010-12-27 13:46:01 +0000

    started to change tag library to not use Ajax for right now

--- trunk/grails-app/taglib/com/grailsPortal/taglib/PortalTagLib.groovy
+++ trunk/grails-app/taglib/com/grailsPortal/taglib/PortalTagLib.groovy
@@ -22,14 +22,36 @@ import com.grailsPortal.domain.ContactPhone
 class PortalTagLib {
 	
 	def contactUtilService
+	def static headingHtml
 	static namespace = 'portal'
+	
+	private String doContactTypesInSelect(contactType){
+		def t="""
+		  <select name="${contactType}">   
+		"""
+		ContactType.list().each {
+		  ContactType ct=it
+		  t+="""	
+		    <option value="${ct.id}">${ct.name}</option>
+		  """
+		}
+		t+="</select>"
+		return t
+	}
 	/**
-	 * This is the heading tag
+	 * This is the heading tag it has one attribute "refresh" which can refresh
+	 * the header since it is only retrieved once from the database.
 	 */
-	def heading={attrs, body-> 
+	def heading={attrs, body->
+		if (headingHtml==null || headingHtml=="" || attrs?.refresh=="Y") { 
 		def ctx = servletContext.getAttribute(GrailsApplicationAttributes.APPLICATION_CONTEXT)
-		def pc=ctx.getBean("portalviewService")
-		out << pc.doHeader(attrs,body())
+		PortalviewService pc=ctx.getBean("portalviewService")
+		headingHtml=pc.doHeader(attrs,body())
+		out<< headingHtml
+		}
+		else{
+			out<< headingHtml
+		}
 	}
 	/**
 	 * tag for displaying the address on the page using the attributes defined.  The view
@@ -56,7 +78,7 @@ class PortalTagLib {
 	 */
 	def showParty={attrs->
 		def id=attrs.id
-		Party instance
+		def instance
 		def fnameValue=""
 		def lnameValue=""
 		if (id!=null && id!=""){
@@ -102,7 +124,7 @@ def phone={attrs->
 def partyId=attrs.id
 def controller=attrs.controller
 def party=Party.get(partyId)
-def contactTypes=contactUtilService.getProfilePhoneContactTypes(party)	
+def contactTypes=ContactType.list()	
 def editAction=attrs.editAction
 def createAction=attrs.createAction	
 if (editAction==null || editAction==""){
@@ -179,15 +201,29 @@ def flowPhone={attrs->
 def partyId=attrs.id
 def controller=attrs.controller
 def party=Party.get(partyId)
-def contactTypes=contactUtilService.getProfilePhoneContactTypes(party)
+List <ContactType> contactTypes=ContactType.list()
 def editAction=attrs.editAction
 def createAction=attrs.createAction
+def contactPhoneId=attrs.contactPhoneId
+def contactPhoneInstance
+if (contactPhoneId==null || contactPhoneId==""){
+	contactPhoneInstance=new ContactPhone()
+	contactPhoneInstance.party=party
+}else{
+  contactPhoneInstance=ContactPhone.get(contactPhoneId)
+}
+def ctId
+if (contactPhoneInstance==null || contactPhoneInstance.contactType==null){
+	ctId=1
+}else{
+  ctId=contactPhoneInstance.contactType.id
+}
 def t="""
 			 <TABLE>
 			  """
 if (party!=null){
    party.phoneList.each{
-	   def phn=it
+	   ContactPhone phn=it
 	   t+="""
 				 <tr class="prop">
 				   <td valign="top" class="name">
@@ -224,16 +260,81 @@ if (party!=null){
 }
 }
 t+="""
-	 <tr><td>Add</td>
- <td>"""
-contactTypes.each{
-def ct=it
-t+="""
-	 ${g.link([controller:"${controller}",action:"${createAction}",params:["contactType.id":"${ct.id}","party.id":"${party.id}"]],"${ct.name}Phone")
-}"""
-}
-t+="""
-   </div>
+	 <tr><td>Phones</td></tr>
+	 <tr><TD>
+            <g:if test="${flash.message}">
+              <div class="message">${flash.message}</div>
+            </g:if>
+            <g:hasErrors bean="${contactPhoneInstance}">
+            <div class="errors">
+                <g:renderErrors bean="${contactPhoneInstance}" as="list" />
+            </div>
+            </g:hasErrors>
+            <g:form action="save" method="post" >
+                <div class="dialog">
+                    <table>
+                        <tbody>
+                        
+                            <tr class="prop">
+                                <td valign="top" class="name">
+                                    <label for="areaCode">Area Code:</label>
+                                </td>
+                                <td valign="top" class="value ${hasErrors(bean:contactPhoneInstance,field:'areaCode','errors')}">
+                                    <input type="text" maxlength="4" id="areaCode" name="areaCode" value="${fieldValue(bean:contactPhoneInstance,field:'areaCode')}"/>
+                                </td>
+                            </tr> 
+                        
+                            <tr class="prop">
+                                <td valign="top" class="name">
+                                    <label for="internationalCode">International Code:</label>
+                                </td>
+                                <td valign="top" class="value ${hasErrors(bean:contactPhoneInstance,field:'internationalCode','errors')}">
+                                    <input type="text" maxlength="2" id="internationalCode" name="internationalCode" value="${fieldValue(bean:contactPhoneInstance,field:'internationalCode')}"/>
+                                </td>
+                            </tr> 
+                        
+                            <tr class="prop">
+                                <td valign="top" class="name">
+                                    <label for="phoneNumber">Phone Number:</label>
+                                </td>
+                                <td valign="top" class="value ${hasErrors(bean:contactPhoneInstance,field:'phoneNumber','errors')}">
+                                    <input type="text" maxlength="20" id="phoneNumber" name="phoneNumber" value="${fieldValue(bean:contactPhoneInstance,field:'phoneNumber')}"/>
+                                </td>
+                            </tr> 
+                        
+                            <tr class="prop">
+                                <td valign="top" class="name">
+                                </td>
+                                <td valign="top"><g:hiddenField name="party" value="${fieldValue(bean:contactPhoneInstance,field:'party')}" /></td>
+                            </tr> 
+                        
+                            <tr class="prop">
+                                <td valign="top" class="name">
+                                    <label for="contactType">Contact Type:</label>
+                                </td>
+                                <td valign="top" class="value ${hasErrors(bean:contactPhoneInstance,field:'contactType','errors')}">
+                                  ${doContactTypesInSelect("phone")}
+                                </td>
+                            </tr> 
+                        
+                            <tr class="prop">
+                                <td valign="top" class="name">
+                                    <label for="active">Active:</label>
+                                </td>
+                                <td valign="top" class="value ${hasErrors(bean:contactPhoneInstance,field:'active','errors')}">
+                                    <g:checkBox name="active" value="${contactPhoneInstance?.active}" ></g:checkBox>
+                                </td>
+                            </tr> 
+                        
+                        </tbody>
+                    </table>
+                </div>
+                <div class="buttons">
+                    <span class="button"><input class="save" type="submit" value="Save" /></span>
+                </div>
+            </g:form>
+            """
+ t+="""
    </td>
    </tr>
   </TABLE>
@@ -403,7 +504,7 @@ party=Party.get(partyId)
 party=new Party()
 }
 def controller=attrs.controller
-def contactTypes=contactUtilService.getProfileEmailContactTypes(party)
+def contactTypes=ContactType.list()
 def editAction=attrs.editAction
 def createAction=attrs.createAction	
 if (editAction==null || editAction==""){
