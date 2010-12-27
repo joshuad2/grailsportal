ee9381af4db532784269e5fb933bb24a0cb9e79a
webtech20 <webtech20@5f668ee0-2ad8-11df-b279-ed2be448802f>
2010-12-27 13:43:54 +0000

    added some tests

---
+++ test/unit/com/grailsPortal/service/config/PortalMenuServiceTests.groovy
@@ -0,0 +1,56 @@
+package com.grailsPortal.service.config
+
+
+class PortalMenuServiceTests extends grails.test.ServiceUnitTestCase{
+/**
+ *  PortalMenuServiceService Unit Test
+ *  @author Joshua Davis    
+ */
+
+   void testshortName=PortalMenuService(){
+	   }
+ 
+   void testpropertyName=portalMenuService(){
+	   }
+ 
+   void testclass=class org.codehaus.groovy.grails.commons.DefaultGrailsServiceClass(){
+	   }
+ 
+   void testavailable=true(){
+	   }
+ 
+   void testpackageName=com.grailsPortal.service.config(){
+	   }
+ 
+   void testnaturalName=Portal Menu Service(){
+	   }
+ 
+   void testpropertyDescriptors=[Ljava.beans.PropertyDescriptor;@12bd56c(){
+	   }
+ 
+   void testlogicalPropertyName=portalMenu(){
+	   }
+ 
+   void testfullName=com.grailsPortal.service.config.PortalMenuService(){
+	   }
+ 
+   void testclazz=class com.grailsPortal.service.config.PortalMenuService(){
+	   }
+ 
+   void testreferenceInstance=com.grailsPortal.service.config.PortalMenuService@c8d74b(){
+	   }
+ 
+   void testmetaClass=groovy.lang.ExpandoMetaClass@12cc880[class com.grailsPortal.service.config.PortalMenuService](){
+	   }
+ 
+   void testtransactional=true(){
+	   }
+ 
+   void testname=PortalMenu(){
+	   }
+ 
+   void testreference=org.springframework.beans.BeanWrapperImpl: wrapping object [com.grailsPortal.service.config.PortalMenuService@11f61a5](){
+	   }
+ 
+}
+
