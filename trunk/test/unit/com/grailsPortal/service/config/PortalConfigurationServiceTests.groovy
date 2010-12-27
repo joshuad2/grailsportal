3ca8403122c23c1106de9c76d46605229655debc
webtech20 <webtech20@5f668ee0-2ad8-11df-b279-ed2be448802f>
2010-12-27 13:43:54 +0000

    added some tests

---
+++ trunk/test/unit/com/grailsPortal/service/config/PortalConfigurationServiceTests.groovy
@@ -0,0 +1,56 @@
+package com.grailsPortal.service.config
+
+
+class PortalConfigurationServiceTests extends grails.test.ServiceUnitTestCase{
+/**
+ *  PortalConfigurationServiceService Unit Test
+ *  @author Joshua Davis    
+ */
+
+   void testshortName=PortalConfigurationService(){
+	   }
+ 
+   void testpropertyName=portalConfigurationService(){
+	   }
+ 
+   void testclass=class org.codehaus.groovy.grails.commons.DefaultGrailsServiceClass(){
+	   }
+ 
+   void testavailable=true(){
+	   }
+ 
+   void testpackageName=com.grailsPortal.service.config(){
+	   }
+ 
+   void testnaturalName=Portal Configuration Service(){
+	   }
+ 
+   void testpropertyDescriptors=[Ljava.beans.PropertyDescriptor;@1f519ae(){
+	   }
+ 
+   void testlogicalPropertyName=portalConfiguration(){
+	   }
+ 
+   void testfullName=com.grailsPortal.service.config.PortalConfigurationService(){
+	   }
+ 
+   void testclazz=class com.grailsPortal.service.config.PortalConfigurationService(){
+	   }
+ 
+   void testreferenceInstance=com.grailsPortal.service.config.PortalConfigurationService@691263(){
+	   }
+ 
+   void testmetaClass=groovy.lang.ExpandoMetaClass@365201[class com.grailsPortal.service.config.PortalConfigurationService](){
+	   }
+ 
+   void testtransactional=true(){
+	   }
+ 
+   void testname=PortalConfiguration(){
+	   }
+ 
+   void testreference=org.springframework.beans.BeanWrapperImpl: wrapping object [com.grailsPortal.service.config.PortalConfigurationService@41e2a3](){
+	   }
+ 
+}
+
